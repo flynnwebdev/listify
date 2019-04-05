9fb87f192fd4321a0b172e7860c9c5d635443953
MattFlynn <flynnwebdev@gmail.com>
2019-04-05 11:13:55 +1000

    Added packing list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,6 +4,12 @@ class ListsController < ApplicationController
       "Bread" => 2,
       "Milk" => 2
     }
-
+  end
+  def packing
+    @clothes = {
+      "T-shirts" => 5,
+      "Socks" => 10,
+      "Ties" => 0
+    }
   end
 end
